543f05b0ed30929e001a6aac99a1547519db495e
SantyCr <hola311271@gmail.com>
2021-11-13 18:23:33 -0500

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+
+
+// Import component
+const routes: Routes = [
+  { 
+    path : " ", // TODO: localhost:4200/
+  }
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  imports: [RouterModule.forRoot(routes)], //Importa declaraciones,componentes, directivas, pipes
+  exports: [RouterModule] //Solo se importan otros modulos
 })
 export class AppRoutingModule { }
